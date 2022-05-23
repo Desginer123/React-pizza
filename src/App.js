a0b8aa59e9be16b02d1cd039eeabb2c8d06743b7
Volodya <hotsru12@gmail.com>
2022-05-23 14:52:36 +0300

    test

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import NotFound from "./pages/NotFound";
+
 const App = () => {
   return (
     <div className="wrapper">
